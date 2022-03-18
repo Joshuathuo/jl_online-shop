84bba103f10d931d04e7f70068ce191eea4ff55b
JM Wainaina <78029949+Joshuathuo@users.noreply.github.com>
2022-03-18 18:30:50 +0300

    Update login.php

--- login.php
+++ login.php
@@ -30,7 +30,7 @@
                                 <h3>LOGIN</h3>
                             </div>
                             <div class="panel-body">
-                                <p>Login to make a purchase.</p>
+                                <p>Login to make a purchase</p>
                                 <form method="post" action="login_submit.php">
                                     <div class="form-group">
                                         <input type="email" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
